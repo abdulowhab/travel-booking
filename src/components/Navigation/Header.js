1641777dc4af131a62a9ff1b57e1f9cf69c6af7b
Md. Abdul Owhab <owhab@pigeon-soft.com>
2022-03-31 21:06:12 +0600

    Created Simple Navbar

--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -1,9 +1,30 @@
 import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { Language } from "@mui/icons-material";
+import { Avatar } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
   return (
-    <div>
-      <h1>This is Header</h1>
+    <div className="header">
+      <img
+        className="header-icon"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
+        alt=""
+      />
+      <div className="header-center">
+        <input type="text" placeholder="Start your search" />
+        <SearchIcon />
+      </div>
+      <div className="header-right">
+        <p>Become a host</p>
+        <Language />
+        <div className="profile">
+          <MenuIcon className="burger-icon" />
+          <Avatar className="" />
+        </div>
+      </div>
     </div>
   );
 };
