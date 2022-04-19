93c4cf8b4b7dd82bd9b4b09d0d0abf2b6a9d4e93
Md. Abdul Owhab <owhab@pigeon-soft.com>
2022-04-19 18:43:21 +0600

    Added Search Bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import "./App.css";
-import Header from "./components/Navigation/Header";
+// import Header from "./components/Navigation/Header";
 
-import FooterDown from "./components/Footer/FooterDown";
-import FooterMiddle from "./components/Footer/FooterMiddle";
-import FooterTop from "./components/Footer/FooterTop";
+// import FooterDown from "./components/Footer/FooterDown";
+// import FooterMiddle from "./components/Footer/FooterMiddle";
+// import FooterTop from "./components/Footer/FooterTop";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Help from "./components/Footer/FooterPages/Help";
-import Home from "./components/Main/Home";
+// import Help from "./components/Footer/FooterPages/Help";
+import Home from "./components/Home/Home";
+// import Home from "./components/Main/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header></Header>
+        {/* <Header></Header> */}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/help" element={<Help />} />
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/help" element={<Help />} /> */}
+
+          <Route  path = "/" element = {<Home/>}/>
         </Routes>
-        <FooterTop />
+        {/* <FooterTop />
         <FooterMiddle />
-        <FooterDown />
+        <FooterDown /> */}
       </BrowserRouter>
     </div>
   );
